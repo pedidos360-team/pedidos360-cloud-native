595b2a737b2ae94e6c53ed51f663f8d63b5a7ec0
Ro <r.jerez@duocuc.cl>
2026-09-15 22:15:25 -0300

    fix: incluir archivos de frontend en el repositorio principal

---
+++ pedidos360-frontend/src/app/components/productos/productos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-productos',
+  styleUrl: './productos.css',
+  templateUrl: './productos.html',
+})
+export class Productos {}
