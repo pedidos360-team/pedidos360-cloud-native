615adbcf80ce826877c12a0a0add3149d6b47289
D-Jaramillo23 <d.jaramillo@duocuc.cl>
2026-09-20 19:33:53 -0300

    config: preparacion inicial para despliegue en Amplify

--- pedidos360-frontend/src/environments/environment.ts
+++ pedidos360-frontend/src/environments/environment.ts
@@ -1,15 +1,14 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8081',
-  ordersApiBaseUrl: 'http://localhost:8082',
+  production: true,
+  apiBaseUrl: 'https://placeholder-api.com',
+  ordersApiBaseUrl: 'https://placeholder-api.com',
   azure: {
     clientId: 'ed614ce8-0c2e-4a39-b0c0-546b14f540df',
     tenantId: 'f526ccbd-b7aa-4640-a83e-9c33ff4bc07e',
     authority: 'https://login.microsoftonline.com/f526ccbd-b7aa-4640-a83e-9c33ff4bc07e',
-    redirectUri: 'http://localhost:4200',
+    redirectUri: 'http://localhost:4200', // Se cambiará por la URL de Amplify al desplegar
     postLogoutRedirectUri: 'http://localhost:4200',
     loginScopes: ['openid', 'profile', 'email'],
-    // Debe coincidir con el scope delegado creado en Entra ID > Expose an API.
     apiScopes: ['api://ed614ce8-0c2e-4a39-b0c0-546b14f540df/access_as_user']
   }
 };
